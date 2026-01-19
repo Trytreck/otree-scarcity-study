655ad57afe941312e3f0a9c0304c83ef8dfd3742
Trytreck <hoathony32@gmail.com>
2026-01-19 17:51:04 +0100

    tests.py

--- Granjo2/tests.py
+++ Granjo2/tests.py
@@ -13,26 +13,23 @@ class PlayerBot(Bot):
         # 2. Page Consentement
         yield Consentement, {'consentement': True}
 
-        # 3. Définition des variables selon la condition
-        # (on prépare les chiffres pour la suite)
-        if self.player.condition == 'social':
+        # 3. Préparation des données selon la condition
+        if self.player.condition == 'quantitatif':
             p_achat, clics, o_min, o_max = 0.9, random.randint(20, 40), 100, 160
-        elif self.player.condition == 'quantitatif':
+        elif self.player.condition == 'social':
             p_achat, clics, o_min, o_max = 0.7, random.randint(10, 25), 85, 130
-        else: # condition 'control'
+        else: # control
             p_achat, clics, o_min, o_max = 0.4, random.randint(2, 10), 60, 95
 
         # 4. Page VenteBillet
         decision_achat = random.random() < p_achat
         yield VenteBillet, {'achat': decision_achat}
 
-        # 5. Page Enchère (Jouée par TOUT LE MONDE)
+        # 5. Page Enchère (Tout le monde la joue)
         if decision_achat:
-            # Si le bot a acheté (achat=True), il met un prix élevé
             offre = random.randint(o_min, o_max)
         else:
-            # Si le bot a dit non (achat=False), il met un prix bas (< 80)
-            offre = random.randint(10, 79)
+            offre = random.randint(10, 79) # Prix bas car refus d'achat à 80
 
         yield Enchere, {
             'offre_enchere': c(offre),
